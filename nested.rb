359520dc28b522f62c3481a5dccd7e1d7cfc0ab8
Eliza Munroe <era3munroe@gmail.com>
2019-07-26 18:32:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -141,7 +141,7 @@ def adding_to_dennis
         }
      }
 
-programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
 
 return programmer_hash
 
