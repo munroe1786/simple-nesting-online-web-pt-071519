c4e82cca00763069c34e5fff4e6e12d965bd3156
Eliza Munroe <era3munroe@gmail.com>
2019-07-26 18:30:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -118,10 +118,11 @@ def changing_alan
   
   programmer_hash[:alan_kay][:known_for] = "GUI"
   
+  return programmer_hash
   
 end
 
-puts programmer_hash
+
 
 def adding_to_dennis
 	programmer_hash = 
